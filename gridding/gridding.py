b6b33ed2bd7640990a6b6b156371c644203cc4c6
Robert Ricker <rori@norceresearch.no>
2024-07-31 14:39:48 +0200

    added function to reorganize netcdf files in year/month directories, switch to csv files instead of geojson

--- gridding/gridding.py
+++ gridding/gridding.py
@@ -6,15 +6,39 @@ import datetime
 import glob
 import re
 import os
+import shutil
 import sys
 import multiprocessing as mp
+from shapely.geometry import Point
 from io_tools import transform_coords
 from io_tools import get_sea_ice_regions
 from io_tools import create_out_dir
+from io_tools import read_dasit_csv
 from gridding.prepare_netcdf import PrepareNetcdf
 from gridding import gridding_lib
 from loguru import logger
 from io_tools import init_logger
+
+
+def organize_files_by_date(source_dir, target_dir):
+    files = [f for f in os.listdir(source_dir) if f.endswith('.nc')]
+    for file in files:
+        match = re.search(r'(\d{8})', file)
+        if match:
+            date_str = match.group(1)
+            year, month = date_str[:4], date_str[4:6]
+            year_dir = os.path.join(target_dir, year)
+            month_dir = os.path.join(year_dir, month)
+            os.makedirs(month_dir, exist_ok=True)
+            src_path = os.path.join(source_dir, file)
+            dest_path = os.path.join(month_dir, file)
+            shutil.move(src_path, dest_path)
+        else:
+            logger.info(f"Date not found in file name: {file}")
+    if not os.listdir(source_dir):
+        os.rmdir(source_dir)
+    else:
+        logger.info(f"Source directory {source_dir} is not empty and has not been removed.")
 
 
 def get_deformation(row):
@@ -44,9 +68,11 @@ def process_file(config, file, grid, region_grid):
     var_range = grd_opt['target_variable_range']["freeboard" if "freeboard" in target_var else "thickness"]
     out_dir = config['dir'][sensor]['netcdf']
 
-    logger.info('process geosjson file: ' + os.path.basename(file))
+    logger.info('process csv file: ' + os.path.basename(file))
 
     data = gpd.read_file(file)
+    data.rename(columns={'sea_ice_thickness_unc': 'sea_ice_thickness_l2_unc'}, inplace=True)
+    # data = read_dasit_csv(file)
     data.crs = "epsg:" + re.findall(r'epsg(\d{4})', os.path.basename(file))[0]
     data.to_crs(crs=out_epsg, inplace=True)
     start_location = data["geometry"].apply(lambda g: g.geoms[0])
@@ -97,7 +123,7 @@ def process_file(config, file, grid, region_grid):
 
     data[target_var+'_total_unc'] = np.sqrt(data[target_var+'_growth_unc']**2 +
                                             data[target_var+'_drift_unc']**2 +
-                                            data[target_var+'_unc']**2)
+                                            data[target_var+'_l2_unc']**2)
 
     data['deformation'] = data.apply(get_deformation, axis=1)
     data['divergence'] = data["divergence"].apply(get_row_mean)
@@ -108,24 +134,28 @@ def process_file(config, file, grid, region_grid):
     master = master.join(tmp_hist_grid.drop(columns=['geometry']))
     master = prepare_netcdf.drop_fields(master)
     centroidseries = master['geometry'].centroid
-    master['yc'], master['xc'] = centroidseries.x, centroidseries.y
+    master['xc'], master['yc'] = round(centroidseries.x), round(centroidseries.y)
     master['time'] = (time_center - datetime.datetime(1970, 1, 1, 0, 0)).total_seconds()
-    master['longitude'], master['latitude'] = transform_coords(master['yc'], master['xc'], out_epsg, 'EPSG:4326')
-    master = master.set_index(['time', 'xc', 'yc'])
+    master['longitude'], master['latitude'] = transform_coords(master['xc'], master['yc'], out_epsg, 'EPSG:4326')
+    master = master.set_index(['time', 'yc', 'xc'])
     master.drop(columns=['geometry'], inplace=True)
     master = xr.Dataset.from_dataframe(master)
+    master = master.reindex(yc=list(reversed(master.yc)))
     master = master.set_coords(("longitude", "latitude"))
     master = prepare_netcdf.add_projection_field(master)
     master["time_bnds"] = xr.DataArray(time_bnds, dims=("time", "nv"), coords={"time": master.time})
-    master['region_flag'] = (['time', 'xc', 'yc'], region_grid[np.newaxis, :, :])
+    master['region_flag'] = (['time', 'yc', 'xc'], region_grid[np.newaxis, :, :])
     hist_arr = xr.concat([master[str(i) + '_sum'] for i in np.arange(hist_n_bins)], dim='zc').values
     master = master.drop_vars([str(i) + '_sum' for i in np.arange(hist_n_bins)])
 
     master = master.assign_coords(zc=("zc", zc))
-    master[target_var + '_hist'] = (['time', 'xc', 'yc', 'zc'], np.transpose(hist_arr, (1, 2, 3, 0)))
+    master[target_var + '_hist'] = (['time', 'yc', 'xc', 'zc'], np.transpose(hist_arr, (1, 2, 3, 0)))
 
     master = prepare_netcdf.set_var_attrbs(master)
     master = prepare_netcdf.set_glob_attrbs(master)
+
+    centr = grid['geometry'][0].centroid
+    distances = [centr.distance(Point(vertex)) for vertex in list(grid['geometry'][0].exterior.coords)]
 
     outfile = (re.split('-', os.path.basename(file))[0] + "-" +
                re.split('-', os.path.basename(file))[1] + "-" +
@@ -133,7 +163,7 @@ def process_file(config, file, grid, region_grid):
                re.split('-', os.path.basename(file))[3] + "-" +
                re.split('-', os.path.basename(file))[4] + "-" +
                'epsg' + out_epsg.split(":")[1] + "_" +
-               "{:.0f}".format(grid.geometry[0].length / 4 / 100.0) + '.nc')
+               "{:.0f}".format(round(min(distances) * np.sqrt(2))/100.0) + '.nc')
 
     comp = dict(zlib=True, complevel=1)
     encoding = {var: comp for var in master.data_vars}
@@ -147,21 +177,30 @@ def gridding(config):
     sensor = config['options']['sensor']
     target_var = config['options']['target_variable']
     grd_opt = config['options']['proc_step_options']['gridding']
-    multiproc = grd_opt['multiproc']
     netcdf_bounds = grd_opt['netcdf_grid']['bounds']
-    geojson_dir = config['dir'][sensor]['geojson'] + grd_opt['geojson_dir']
-    file_list = sorted(glob.glob(geojson_dir + "/" + target_var + '*.geojson'))
+    if grd_opt['csv_dir'] == "all":
+        file_list = sorted([os.path.join(root, file)
+                            for root, _, files in os.walk(config['dir'][sensor]['csv'])
+                            for file in files
+                            if file.endswith('.geojson')])
+                            # if file.endswith('.csv')])
+    else:
+        csv_dir = config['dir'][sensor]['csv'] + grd_opt['csv_dir']
+        file_list = sorted(glob.glob(csv_dir + "/" + target_var + '*.geojson'))
+        # file_list = sorted(glob.glob(csv_dir + "/" + target_var + '*.csv'))
 
     grid, cell_width = gridding_lib.define_grid(
         netcdf_bounds,
         grd_opt['netcdf_grid']['dim'],
-        config['options']['out_epsg'])
+        config['options']['out_epsg'],
+        grid_type='circular')
 
     config['dir'][sensor]['netcdf'] = create_out_dir(config, config['dir'][sensor]['netcdf'], cell_width)
-    region_grid = get_sea_ice_regions(config['dir']['auxiliary']['reg_mask'], netcdf_bounds, cell_width,
+    region_grid = get_sea_ice_regions(config['dir']['auxiliary']['reg_mask'], netcdf_bounds,
+                                      round(0.5 * np.sqrt(2) * cell_width),
                                       config['options']['out_epsg'])
 
-    if multiproc:
+    if grd_opt['multiproc']:
         logger.info('start multiprocessing')
         pool = mp.Pool()
         for file in file_list:
@@ -171,3 +210,6 @@ def gridding(config):
     else:
         for file in file_list:
             process_file(config, file, grid, region_grid)
+
+    if grd_opt["organize_files"]:
+        organize_files_by_date(config['dir'][sensor]['netcdf'], os.path.dirname(config['dir'][sensor]['netcdf']))
