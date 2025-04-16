82d1c5a59b9b2ea278ed441785485b8a4c93cb02
Marion Bocquet <mabo@norceresearch.no>
2025-04-16 16:32:02 +0200

    fix bugs in the satellite value count

--- stacking/drift_aware_processor.py
+++ stacking/drift_aware_processor.py
@@ -75,13 +75,13 @@ class DriftAwareProcessor:
             add_grid = gridding_lib.grid_data(sit, self.grid, self.add_variable+['time'],
                                               self.add_variable+['time'], agg_mode=['mean'])
             frac_mission_grid = gridding_lib.grid_data(sit, self.grid, self.sensor,
-                                              self.sensor, agg_mode=['mean', 'cnt'])
+                                              self.sensor, agg_mode=['mean', 'sum'])
             
             tmp_grid[self.target_var+'_l2_unc'] = np.sqrt(unc_grid[self.target_var+'_l2_unc_sum'])/unc_grid[
                 self.target_var+'_l2_unc_cnt']
             tmp_grid[self.add_variable] = add_grid[self.add_variable]
             tmp_grid[self.sensor] = frac_mission_grid[self.sensor]
-            tmp_grid[[s + '_cnt' for s in self.sensor]] = frac_mission_grid[[s + '_cnt' for s in self.sensor]]
+            tmp_grid[[s + '_cnt' for s in self.sensor]] = frac_mission_grid[[s + '_sum' for s in self.sensor]]
             tmp_grid['t0'] = add_grid['time']
             tmp_grid['xu'] = tmp_grid.index.get_level_values('x')
             tmp_grid['yu'] = tmp_grid.index.get_level_values('y')
