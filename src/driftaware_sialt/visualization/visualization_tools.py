5758ed7e29a7b8f5276ed9eed799ee59c606c8d5
Robert Ricker <rori@norceresearch.no>
2026-07-28 15:43:52 +0200

    major restructuring of the visualization routines

--- src/driftaware_sialt/visualization/visualization_tools.py
+++ src/driftaware_sialt/visualization/visualization_tools.py
@@ -1,67 +1,103 @@
+import os
+import subprocess
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
-import os
-import subprocess
-from matplotlib.colors import ListedColormap
 import cartopy.feature as cfeature
 
 
-def visu_xarray(x, y, z, figsize, vmin, vmax, n_level, cmap, time_string, label, outfile, hem, iceconc=None):
+def _projection_and_extent(hem):
     if hem == 'nh':
-        crs = ccrs.LambertAzimuthalEqualArea(central_longitude=0.0, central_latitude=90.0, false_easting=0.0,
-                                             false_northing=0.0)
+        central_latitude = 90.0
+        extent = [-4150000, 3000000, -3150000, 4000000]
     else:
-        crs = ccrs.LambertAzimuthalEqualArea(central_longitude=0.0, central_latitude=-90.0, false_easting=0.0,
-                                             false_northing=0.0)
-    fig = plt.figure(figsize=figsize)
+        central_latitude = -90.0
+        extent = [-3450000, 3850000, -3200000, 4100000]
 
-    xc, yc = np.meshgrid(x, y)
-    fontsize = 18
+    projection = ccrs.LambertAzimuthalEqualArea(
+        central_longitude=0.0,
+        central_latitude=central_latitude,
+        false_easting=0.0,
+        false_northing=0.0,
+    )
+    return projection, extent
 
-    bounds = list(np.linspace(vmin, vmax, num=n_level))
-    norm = matplotlib.colors.BoundaryNorm(bounds, cmap.N, extend='neither')
-    ax = plt.subplot(projection=crs)
-    if hem=='nh':
-        ax.set_extent([-3850000, 3000000, -3000000, 3850000], crs=crs)
-    else:
-        ax.set_extent([-3700000, 3700000, -3150000, 4100000], crs=crs)
-    if iceconc:
-        if len(iceconc) > 0:
-            iceconc["ice_conc"][iceconc["ice_conc"] < 15.0] = np.nan
-            iceconc["ice_conc"][iceconc["ice_conc"] >= 15.0] = 1.0
-            ax.pcolormesh(xc, yc, iceconc["ice_conc"], cmap=ListedColormap([(1, 1, 1)]))
 
-    im = ax.pcolormesh(xc, yc, z, cmap=cmap, norm=norm)
-    #ax.add_feature(cartopy.feature.OCEAN, facecolor=(0.86, 0.87, 0.9))
-    coastline = cfeature.NaturalEarthFeature('physical', 'coastline', '50m',
-                                             facecolor=(0.73, 0.74, 0.75), edgecolor='black')
-    shelves = cfeature.NaturalEarthFeature('physical', 'antarctic_ice_shelves_polys', '50m',
-                                             facecolor='lightgray', edgecolor='lightgray')
-    ax.add_feature(shelves, linewidth=0.2)
-    ax.add_feature(coastline, linewidth=0.2)
-    ax.axis("off")
+def _add_map_features(ax):
+    ax.set_facecolor("white")
+    coastline = cfeature.NaturalEarthFeature(
+        'physical',
+        'coastline',
+        '50m',
+        facecolor=(0.95, 0.95, 0.95),
+        edgecolor='dimgrey',
+    )
+    shelves = cfeature.NaturalEarthFeature(
+        'physical',
+        'antarctic_ice_shelves_polys',
+        '50m',
+        facecolor='lightgray',
+        edgecolor='lightgray',
+    )
+    ax.add_feature(shelves, linewidth=0.4)
+    ax.add_feature(coastline, linewidth=0.4)
+
+
+def _add_date_annotation(ax, time_string, fontsize):
     plt.annotate(
         f"{time_string[0:4]}-{time_string[4:6]}-{time_string[6:8]}",
-        xy=(0.01, 0.95),
+        xy=(0.0, 0.0),
         fontsize=fontsize,
         xycoords='axes fraction',
-        color='black')
+        color='black',
+    )
 
-    lon = np.linspace(0, 2 * np.pi, 100) * 180 / np.pi
-    if hem=='nh':
-        ax.plot(lon, 88.0 * np.ones_like(lon), transform=ccrs.Geodetic(), linestyle='--', color='black', linewidth=1.0)
-    cax = ax.inset_axes([0, -0.05, 1, 0.025], transform=ax.transAxes)
-    cb = plt.colorbar(im, ax=ax, orientation='horizontal', cax=cax)
+
+def _add_colorbar(ax, image, label, fontsize):
+    cax = ax.inset_axes([0, -0.08, 1, 0.035], transform=ax.transAxes)
+    cb = plt.colorbar(image, ax=ax, orientation='horizontal', cax=cax)
     cb.set_label(label=label, size=fontsize, color='black')
     cb.ax.tick_params(labelsize=fontsize, color='black', labelcolor='black')
     cb.ax.tick_params(which='both', length=0)
     cb.outline.set_linewidth(0)
     cb.ax.xaxis.set_major_locator(plt.MaxNLocator(2))
-    fig.savefig(outfile, bbox_inches='tight', pad_inches=0, dpi=180)
-    
+
+
+def visu_xarray(
+    x,
+    y,
+    z,
+    figsize,
+    vmin,
+    vmax,
+    n_level,
+    cmap,
+    time_string,
+    label,
+    outfile,
+    hem,
+):
+    """Plot a gridded variable on a polar map and save it to ``outfile``."""
+    projection, extent = _projection_and_extent(hem)
+    fig = plt.figure(figsize=figsize)
+    ax = plt.subplot(projection=projection)
+    ax.set_extent(extent, crs=projection)
+
+    xc, yc = np.meshgrid(x, y)
+    bounds = list(np.linspace(vmin, vmax, num=n_level))
+    norm = matplotlib.colors.BoundaryNorm(bounds, cmap.N, extend='neither')
+
+    image = ax.pcolormesh(xc, yc, z, cmap=cmap, norm=norm)
+    _add_map_features(ax)
+    ax.axis("off")
+
+    fontsize = 16
+    _add_date_annotation(ax, time_string, fontsize)
+    _add_colorbar(ax, image, label, fontsize)
+
+    fig.savefig(outfile, bbox_inches='tight', pad_inches=0, dpi=200)
     plt.close(fig)
 
 
